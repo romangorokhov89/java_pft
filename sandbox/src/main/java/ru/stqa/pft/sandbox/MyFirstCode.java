4de77f37138d2a739960c92d5f9864c6985abe3d
Горохов Роман Сергеевич <rsgorokhov@stream.local>
2021-12-05 20:03:48 +0300

    Поигрался с отступами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
@@ -1,8 +1,9 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstCode {
-	public static void main(String[] args) {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
 
-		System.out.println("Hello, world");
-	}
 }
