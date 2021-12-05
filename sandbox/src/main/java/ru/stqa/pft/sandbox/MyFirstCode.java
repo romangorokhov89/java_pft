3c6f315defce328cc4aa0f5490596bf06ecba19e
Горохов Роман Сергеевич <rsgorokhov@stream.local>
2021-12-05 23:40:42 +0300

    Добавил несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
@@ -3,12 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstCode {
   
   public static void main(String[] args) {
-    String somebody = "World";
-    System.out.println("Hello, " + somebody + "!");
+    hello("world");
+    hello("Roman");
+    hello("Kseniia");
 
     double l = 8.46;
-    double s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " равна = " + s);
+    System.out.println("Площадь квадрата со стороной " + l + " равна = " + area(l));
+
+    double a = 4;
+    double b = 5.5;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна = " + area(a, b));
+  }
+
+
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len){
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
   }
 
 }
