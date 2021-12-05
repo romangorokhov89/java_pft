636764bcead9f01fcd350411bcb025b7b336be0e
romangorokhov89 <romangorokhov89@yandex.ru>
2021-12-05 21:41:59 +0300

    Добавил переменные и высчитывание площади квадрата

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstCode {
   
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    String somebody = "World";
+    System.out.println("Hello, " + somebody + "!");
+
+    double l = 8.46;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " равна = " + s);
   }
 
 }
