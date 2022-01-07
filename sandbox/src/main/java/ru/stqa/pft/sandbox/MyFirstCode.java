b9c4dd4bfc43a89e9c750ccab29daef89304c173
Горохов Роман Сергеевич <rsgorokhov@stream.local>
2022-01-07 17:41:12 +0300

    Добавлены методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstCode.java
@@ -1,31 +1,22 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstCode {
-  
+
   public static void main(String[] args) {
     hello("world");
     hello("Roman");
     hello("Kseniia");
 
     Square s = new Square(8.5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " равна = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна = " + s.area());
 
     Rectangle r = new Rectangle(5, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна = " + r.area());
   }
-
 
 
   public static void hello(String somebody) {
     System.out.println("Hello " + somebody + "!");
-  }
-
-  public static double area(Square s){
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r){
-    return r.a * r.b;
   }
 
 }
